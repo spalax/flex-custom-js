4b4bdc8edc95ff6b66f99d5809b0679b6e9d603a
Oleksii Mylotskyi <spalaxinco@gmail.com>
2019-12-01 17:17:02 +0200

    Added benchmark for solutions

---
+++ index-benchmark.js
@@ -0,0 +1,111 @@
+const {VM, VMScript} = require('vm2');
+const userContext = {'ip': '185.128.156.162'};
+
+const untrusted5 = `
+	async function onRequest (request, response) {
+		return "myhost" + (request * response) + ".com";
+	}
+`;
+
+const wrapper = `
+    (async () => {
+		const res = await onRequest(global.getRequest(), global.getResponse());
+		if (Array.isArray(res)) {
+			return res.filter((item) => (typeof res === 'string' || res instanceof String));
+		} else if (typeof res === 'string' || res instanceof String) {
+			return res;
+		} else {
+		   return false;
+		}
+    })();
+`;
+
+async function fast() {
+    console.time("fast-solution");
+    const context = {request: 123, response: 2};
+    const sandbox = {
+        getRequest: () => {
+            return context.request
+        }, getResponse: () => {
+            return context.response
+        }
+    };
+    const vm = new VM({
+        wasm: false,
+        console: 'inherit',
+        eval: false,
+        timeout: 1000,
+        sandbox: sandbox
+    });
+    const promises = [];
+    const codeToRun = new VMScript(untrusted5).wrap(
+        '',
+        wrapper
+    ).compile();
+    (async () => {
+        for (let i = 0; i < 10000; i++) {
+            context.request += i;
+            await vm.run(codeToRun);
+        }
+        console.timeEnd("fast-solution");
+    })();
+}
+
+async function blazingFast() {
+    console.time("blazing-fast-solution");
+    const context = {request: 123, response: 2};
+    const sandbox = {
+        getRequest: () => {
+            return context.request
+        }, getResponse: () => {
+            return context.response
+        }
+    };
+    const vm = new VM({
+        wasm: false,
+        console: 'inherit',
+        eval: false,
+        timeout: 1000,
+        sandbox: sandbox
+    });
+    const codeToRun = new VMScript(untrusted5).wrap(
+        '',
+        wrapper
+    ).compile();
+    const promises = [];
+    for (let i = 0; i < 10000; i++) {
+        context.request += i;
+        promises.push(vm.run(codeToRun));
+    }
+    Promise.all(promises).then(() => {
+        console.timeEnd("blazing-fast-solution");
+    })
+}
+
+async function currentSolution() {
+    console.time("current-solution");
+    const codeToRun = new VMScript(untrusted5).wrap(
+        '',
+        wrapper
+    ).compile();
+    for (let i = 0; i < 10000; i++) {
+        const vm = new VM({
+            wasm: false,
+            console: 'inherit',
+            eval: false,
+            timeout: 1000,
+            sandbox: {
+                getRequest: () => 123 + i,
+                getResponse:() => 2
+            }
+        });
+        await vm.run(codeToRun);
+    }
+    console.timeEnd("current-solution");
+}
+
+(async () => {
+    await currentSolution();
+    await fast();
+    await blazingFast();
+})();
